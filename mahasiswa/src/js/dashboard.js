714a70166bb838048b383fe604595a3d7651c6c6
dj1samsoe <dhanyh86@gmail.com>
2024-04-07 11:48:48 +0700

    style: fix style sidebar active routes

--- mahasiswa/src/js/dashboard.js
+++ mahasiswa/src/js/dashboard.js
@@ -1,28 +1,96 @@
-// Function to load the navigation
-function loadNavigation() {
-  fetch("./components/sidebar/sidebar.html")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Failed to load sidebar.html");
-      }
-      return response.text();
-    })
-    .then((html) => {
-      // Inject the HTML content into the sidebar-menu div
-      document.getElementById("sidebar-menu").innerHTML = html;
+// // Function to load the navigation
+// function loadNavigation() {
+//   fetch("./components/sidebar/sidebar.html")
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error("Failed to load sidebar.html");
+//       }
+//       return response.text();
+//     })
+//     .then((html) => {
+//       // Inject the HTML content into the sidebar-menu div
+//       document.getElementById("sidebar-menu").innerHTML = html;
 
-      // Execute script from myComponent.js
-      var scriptElement = document.createElement("script");
-      scriptElement.src = "./components/sidebar/sidebar.js";
-      document.body.appendChild(scriptElement);
-    })
-    .catch((error) => {
-      console.error("Error loading navigation:", error);
-    });
+//       // Execute script from myComponent.js
+//       var scriptElement = document.createElement("script");
+//       scriptElement.src = "./components/sidebar/sidebar.js";
+//       document.body.appendChild(scriptElement);
+//     })
+//     .catch((error) => {
+//       console.error("Error loading navigation:", error);
+//     });
+// }
+
+// // Call the function to load the navigation when the page loads
+// window.addEventListener("DOMContentLoaded", loadNavigation);
+
+// Get the button and dropdown menu
+const btnKuliah = document.getElementById("perkuliahan");
+const btnTugas = document.getElementById("tugas");
+const arrowKuliah = document.getElementById("arrow-kuliah");
+const arrowTugas = document.getElementById("arrow-tugas");
+const dropdownKuliah = document.querySelector(".dropdown-kuliah");
+const dropdownTugas = document.querySelector(".dropdown-tugas");
+
+// Function to toggle dropdown menu visibility for Kuliah
+function toggleDropdownKuliah() {
+  const isOpen = !dropdownKuliah.classList.contains("open");
+  dropdownKuliah.classList.toggle("open", isOpen);
+  arrowKuliah.classList.toggle("-rotate-180", isOpen);
+  localStorage.setItem("dropdownKuliahOpen", isOpen ? "true" : "false");
 }
 
-// Call the function to load the navigation when the page loads
-window.addEventListener("DOMContentLoaded", loadNavigation);
+// Function to toggle dropdown menu visibility for Tugas
+function toggleDropdownTugas() {
+  const isOpen = !dropdownTugas.classList.contains("open");
+  dropdownTugas.classList.toggle("open", isOpen);
+  arrowTugas.classList.toggle("-rotate-180", isOpen);
+  localStorage.setItem("dropdownTugasOpen", isOpen ? "true" : "false");
+}
+
+// Add event listener to the button to toggle dropdown menu visibility for Kuliah
+btnKuliah.addEventListener("click", toggleDropdownKuliah);
+btnTugas.addEventListener("click", toggleDropdownTugas);
+
+// Load the state of the dropdown menus from local storage
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdownKuliahOpen =
+    localStorage.getItem("dropdownKuliahOpen") === "true";
+  const dropdownTugasOpen =
+    localStorage.getItem("dropdownTugasOpen") === "true";
+
+  if (dropdownKuliahOpen) {
+    dropdownKuliah.classList.add("open");
+    arrowKuliah.classList.add("-rotate-180");
+  }
+
+  if (dropdownTugasOpen) {
+    dropdownTugas.classList.add("open");
+    arrowTugas.classList.add("-rotate-180");
+  }
+});
+
+const linkDashboard = document.getElementById("link-dashboard");
+
+linkDashboard.addEventListener("click", function () {
+  window.location.href = "dashboard.html";
+});
+
+// for active link routes
+document.addEventListener("DOMContentLoaded", function () {
+  const currentLocation = window.location.pathname;
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  navLinks.forEach((link) => {
+    const hiddenDiv = link.querySelector(".hidden");
+    if (link.pathname === currentLocation) {
+      link.classList.add("active");
+      if (hiddenDiv) {
+        hiddenDiv.classList.remove("hidden");
+      }
+    }
+  });
+});
 
 // Data for the bar chart
 const data = {
