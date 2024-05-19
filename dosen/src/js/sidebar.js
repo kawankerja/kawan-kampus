778f24ac7ffa0affca89e8a4182b5f02bc7e8d63
aria <nurhibnastiar@gmail.com>
2024-05-19 18:41:54 +0700

    add : role - dashboard

---
+++ dosen/src/js/sidebar.js
@@ -0,0 +1,9 @@
+document.getElementById("profile").addEventListener("click", function () {
+	document.getElementById("dropdown-profile").classList.toggle("hidden");
+	document.getElementById("icon-profile").classList.toggle("rotate-180");
+});
+
+document.getElementById("bimbingan").addEventListener("click", function () {
+	document.getElementById("dropdown-bimbingan").classList.toggle("hidden");
+	document.getElementById("icon-bimbingan").classList.toggle("rotate-180");
+});
